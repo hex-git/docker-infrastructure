f8645df39a9456c98aaa8d44f0bd19da42252558
HeX <105430506+hex-git@users.noreply.github.com>
2022-05-18 17:01:13 +0300

    Update entrypoint.sh
    restart ability

--- no-conda-runner/entrypoint.sh
+++ no-conda-runner/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -eu
 
+if [ ! -f ./registered]; then
+# register if not
 /no-conda-runner/config.sh --url https://github.com/hex-git/$REPO --token $GH_TOKEN
+touch ./registered
+fi
 while true; do /no-conda-runner/run.sh; sleep 10; done
