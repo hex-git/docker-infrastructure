5506b211ee8d13dc58ad4589a88d7c68deab4934
HeX <105430506+hex-git@users.noreply.github.com>
2022-05-18 17:00:04 +0300

    Update entrypoint.sh
    fixing restart

--- base-ghrunner/entrypoint.sh
+++ base-ghrunner/entrypoint.sh
@@ -2,5 +2,9 @@
 set -eu
 
 source $CONDA/bin/activate
+if [ ! -f ./registered]; then
+# register if not
 /base-runner/config.sh --url https://github.com/hex-git/$REPO --token $GH_TOKEN
+touch ./registered
+fi
 while true; do /base-runner/run.sh; sleep 10; done
