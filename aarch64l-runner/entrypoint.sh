c49d0e562e119d952f74c6d1932e15064767bdc6
HeX <105430506+hex-git@users.noreply.github.com>
2022-07-12 14:08:11 +0300

    Update entrypoint.sh
    little fix

--- aarch64l-runner/entrypoint.sh
+++ aarch64l-runner/entrypoint.sh
@@ -4,7 +4,7 @@ set -eu
 source $CONDA/bin/activate
 if [ ! -f ./registered]; then
 # register if not
-/base-runner/config.sh --url https://github.com/hex-git/$REPO --token $(python3 -c /base-runner/get_token.py)
+/base-runner/config.sh --url https://github.com/hex-git/$REPO --token $(python3 /base-runner/get_token.py)
 touch ./registered
 fi
 while true; do /base-runner/run.sh; sleep 10; done
